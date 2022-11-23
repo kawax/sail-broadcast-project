1e63c58ce8f350f679b390ec4d7e9f8c32235d69
kawax <kawaxbiz@gmail.com>
2022-11-23 12:26:02 +0900

    wip

--- app/Http/Controllers/PresenceController.php
+++ app/Http/Controllers/PresenceController.php
@@ -11,12 +11,10 @@ class PresenceController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @return void
      */
     public function __invoke(Request $request)
     {
         PresenceSubmit::broadcast($request->user()->name, $request->input('chat'));
-
-        return to_route('dashboard');
     }
 }
--- app/Http/Controllers/PrivateController.php
+++ app/Http/Controllers/PrivateController.php
@@ -11,12 +11,10 @@ class PrivateController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @return void
      */
     public function __invoke(Request $request)
     {
         PrivateSubmit::broadcast($request->user()->id, $request->user()->name, $request->input('chat'));
-
-        return to_route('dashboard');
     }
 }
