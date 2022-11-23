9b73eaee9d409dd90773768b45c93de22053beac
kawax <kawaxbiz@gmail.com>
2022-11-23 10:36:11 +0900

    PresenceChannel

--- app/Http/Controllers/PresenceController.php
+++ app/Http/Controllers/PresenceController.php
@@ -11,10 +11,12 @@ class PresenceController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Inertia\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function __invoke(Request $request)
     {
         PresenceSubmit::broadcast($request->user()->name, $request->input('chat'));
+
+        return to_route('dashboard');
     }
 }
